d053ff7d9e94c3ba552bde1c6376959825903576
javier-manas <javiermbuf@gmail.com>
2026-03-28 21:03:44 +0100

    Ultimos cambios

--- evaluador/evaluadorNew.py
+++ evaluador/evaluadorNew.py
@@ -1,11 +1,7 @@
 import re
-import numpy as np
 from transformers import pipeline
-import json
 import pandas as pd
 import os
-
-from collections import Counter
 
 
 
---
+++ pruebas/pruebasJuan.py
@@ -0,0 +1,6 @@
+import re
+import numpy as np
+from transformers import pipeline
+import json
+import pandas as pd
+import os
